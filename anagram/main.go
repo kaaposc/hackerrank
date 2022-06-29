2100f3aaef177e9a660a3f8e3839f0da293648c9
Māris Vilks <kaaposc@gmail.com>
2022-06-29 17:28:46 +0300

    Simplified, not using slices

--- anagram/main.go
+++ anagram/main.go
@@ -1,20 +1,16 @@
 package anagram
 
-import (
-	"strings"
-)
-
 func anagram(s string) int32 {
 	if len(s)%2 == 1 {
 		return -1
 	}
 
-	left := strings.Split(s[:len(s)/2], "")
-	right := strings.Split(s[len(s)/2:], "")
+	left := s[:len(s)/2]
+	right := s[len(s)/2:]
 
-	var search = func(s []string, needle string) int {
+	var search = func(s string, r rune) int {
 		for i, ch := range s {
-			if ch == needle {
+			if ch == r {
 				return i
 			}
 		}
@@ -24,7 +20,7 @@ func anagram(s string) int32 {
 
 	for _, ch := range left {
 		if p := search(right, ch); p != -1 {
-			right = append(right[:p], right[p+1:]...)
+			right = right[:p] + right[p+1:]
 		}
 	}
 
